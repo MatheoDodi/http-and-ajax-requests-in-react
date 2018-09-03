6f917133299b029f6793a5dd75aeb2c8a0416719
MatthewDodi <matthew.dodi@gmail.com>
2018-09-03 00:35:38 -0700

    implemented a deletePostHandler that deletes the selected post from the server

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -19,6 +19,13 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler = () => {
+        axios.delete('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
 
     render () {
         let post = <p style={{textAlign : 'center' }} >Please select a Post!</p>;
@@ -32,7 +39,7 @@ class FullPost extends Component {
                     <h1>{this.state.post.title}</h1>
                     <p>{this.state.post.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
